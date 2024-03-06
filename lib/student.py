68a2c23499cdfadaf03034d8582a75d545fb26b2
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-03-06 19:02:02 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+
+    def learn(self, new_knowledge):
+        if isinstance(new_knowledge, str):
+            self.knowledge.append(new_knowledge)
+        
